fe8e891decf2c94b5f8ecadf61635f4a55f6eadb
John Emilio <jl.emilio1@gmail.com>
2022-08-19 19:28:55 -0400

    changes to server

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ connectDB()
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname, 'client', 'public', 'index.html')))
 
 // Routes
 app.use('/', homeRoutes)
