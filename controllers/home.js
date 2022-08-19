a796995294c56b3c9b4fe070f114fe51cad11039
John Emilio <jl.emilio1@gmail.com>
2022-08-19 19:31:39 -0400

    changes to server

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,7 @@
 module.exports = {
     getIndex: (req, res) => {
-        res.send('Hello from the backend')
+        // res.send('Hello from the backend')
+        res.render('index')
     },
     logout: async (req, res) => {
         res.send({ msg: false })
