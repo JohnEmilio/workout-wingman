4820ef5adb5782dc055f3df3a4befd19e114b16e
John Emilio <jl.emilio1@gmail.com>
2022-08-19 16:58:26 -0400

    heroku changes

--- controllers/home.js
+++ controllers/home.js
@@ -1,6 +1,6 @@
 module.exports = {
     getIndex: (req, res) => {
-        res.send('Hello from the backend')
+        res.render('index.html')
     },
     logout: async (req, res) => {
         res.send({ msg: false })
