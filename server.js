1645ae324493d4fa15886844c738ee264832cf03
John Emilio <jl.emilio1@gmail.com>
2022-08-20 09:45:59 -0400

    server changes

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/user', userRoutes)
 // }
 // console.log(process.env.NODE_ENV)
 
-app.use(express.static(path.resolve(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, './client/build')))
 
 // app.get('*', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'client', 'build'))
